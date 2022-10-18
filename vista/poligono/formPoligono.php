3b6aa2dde6b410e971b5ead8bfdc70e555fe8f63
brunotecnqn <91470629+brunotecnqn@users.noreply.github.com>
2022-10-18 13:06:12 -0300

    Corrección para ingresar numeros decimales con mas de dos cifras y detalles de validación.

--- vista/poligono/formPoligono.php
+++ vista/poligono/formPoligono.php
@@ -17,86 +17,54 @@ $lon4=-2.59;
     <h3 class="text-center">Poligono</h3>
     <form method="post" class="row m-3 p-4 pt-3 bg-light fw-bold needs-validation" action="accion/accionPoligono.php" novalidate>
 
-    <div class="col-md-3 border border-secondary pb-2">
+    <div class="col-md-3 border border-secondary  rounded pb-2">
 
-<label for="coordenadas[0][latitud]" class="form-label">Latitud (min:-90 y max:90)</label>
-<input type="number" class="form-control" name="coordenadas[0][latitud]" min="-90" max="90" step='0.01000' value="<?php echo $lat1; ?>" required>
-<div class="invalid-feedback">
-    Ingrese valor min:-90 y max:90
-</div>
-<div class="valid-feedback">
-    Correcto!
-</div>
+<label for="coordenadas[0][latitud]" class="form-label">Latitud</label>
+<input type="number" class="form-control" name="coordenadas[0][latitud]" min="-90" max="90" step='any' value="<?php echo $lat1; ?>" required>
 
-<label for="coordenadas[0][longitud]" class="form-label">Longitud (min:-180 y max:180)</label>
-<input type="number" class="form-control" name="coordenadas[0][longitud]" min="-180" max="180" step='0.01' value="<?php echo $lon1; ?>" required>
-<div class="invalid-feedback">
-    Ingrese valor min:-180 y max:180
-</div>
-<div class="valid-feedback">
-    Correcto!
-</div>
+
+
+<label for="coordenadas[0][longitud]" class="form-label">Longitud </label>
+<input type="number" class="form-control" name="coordenadas[0][longitud]" min="-180" max="180" step='any' value="<?php echo $lon1; ?>" required>
+
+
 </div>
 
 <div class="col-md-3  border border-secondary">
 
-<label for="coordenadas[1][latitud]" class="form-label">Latitud (min:-90 y max:90)</label>
-<input type="number" class="form-control" name="coordenadas[1][latitud]" min="-90" max="90" value="<?php echo $lat2; ?>" step='0.01' required>
-<div class="invalid-feedback">
-    Ingrese valor min:-90 y max:90
-</div>
-<div class="valid-feedback">
-    Correcto!
-</div>
+<label for="coordenadas[1][latitud]" class="form-label">Latitud</label>
+<input type="number" class="form-control" name="coordenadas[1][latitud]" min="-90" max="90" value="<?php echo $lat2; ?>" step='any' required>
 
-<label for="coordenadas[1][longitud]" class="form-label">Longitud (min:-180 y max:180)</label>
-<input type="number" class="form-control" name="coordenadas[1][longitud]" min="-180" max="180" value="<?php echo $lon2; ?>" step='0.01' required>
-<div class="invalid-feedback">
-    Ingrese valor min:-180 y max:180
-</div>
-<div class="valid-feedback">
-    Correcto!
-</div>
+
+
+<label for="coordenadas[1][longitud]" class="form-label">Longitud</label>
+<input type="number" class="form-control" name="coordenadas[1][longitud]" min="-180" max="180" value="<?php echo $lon2; ?>" step='any' required>
+
+
 </div>
 <div class="col-md-3 border border-secondary pb-2">
 
-<label for="coordenadas[2][latitud]" class="form-label">Latitud (min:-90 y max:90)</label>
-<input type="number" class="form-control" name="coordenadas[2][latitud]" min="-90" max="90" value="<?php echo $lat3; ?>" step='0.01' required>
-<div class="invalid-feedback">
-Ingrese valor min:-90 y max:90
-</div>
-<div class="valid-feedback">
-Correcto!
-</div>
+<label for="coordenadas[2][latitud]" class="form-label">Latitud </label>
+<input type="number" class="form-control" name="coordenadas[2][latitud]" min="-90" max="90" value="<?php echo $lat3; ?>" step='any' required>
 
-<label for="coordenadas[2][longitud]" class="form-label">Longitud (min:-180 y max:180)</label>
-<input type="number" class="form-control" name="coordenadas[2][longitud]" min="-180" max="180" step='0.01' value="<?php echo $lon1; ?>" required>
-<div class="invalid-feedback">
-Ingrese valor min:-180 y max:180
-</div>
-<div class="valid-feedback">
-Correcto!
-</div>
+
+
+<label for="coordenadas[2][longitud]" class="form-label">Longitud </label>
+<input type="number" class="form-control" name="coordenadas[2][longitud]" min="-180" max="180" step='any' value="<?php echo $lon1; ?>" required>
+
+
 </div>
 <div class="col-md-3 border border-secondary pb-2">
 
-<label for="coordenadas[3][latitud]" class="form-label">Latitud (min:-90 y max:90)</label>
-<input type="number" class="form-control" name="coordenadas[3][latitud]" min="-90" max="90" step='0.01' value="<?php echo $lat4; ?>" required>
-<div class="invalid-feedback">
-    Ingrese valor min:-90 y max:90
-</div>
-<div class="valid-feedback">
-    Correcto!
-</div>
+<label for="coordenadas[3][latitud]" class="form-label">Latitud </label>
+<input type="number" class="form-control" name="coordenadas[3][latitud]" min="-90" max="90" step='any' value="<?php echo $lat4; ?>" required>
 
-<label for="coordenadas[3][longitud]" class="form-label">Longitud (min:-180 y max:180)</label>
-<input type="number" class="form-control" name="coordenadas[3][longitud]" min="-180" max="180" value="<?php echo $lon4; ?>" step='0.01' required>
-<div class="invalid-feedback">
-    Ingrese valor min:-180 y max:180
-</div>
-<div class="valid-feedback">
-    Correcto!
-</div>
+
+
+<label for="coordenadas[3][longitud]" class="form-label">Longitud</label>
+<input type="number" class="form-control" name="coordenadas[3][longitud]" min="-180" max="180" value="<?php echo $lon4; ?>" step='any' required>
+
+
 </div>
 
         <input id="accion" name="accion" value="nuevo" type="hidden">
